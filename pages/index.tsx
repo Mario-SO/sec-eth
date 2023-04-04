af0bd2d1f7c287fcb4902632f90d3f1239f5eec1
Mario S.O <gh_mario.proclaim@slmail.me>
2023-04-04 02:35:36 -0400

    favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,19 @@ import Thread from "./components/Thread";
 import data from "../data/data.json";
 
 import { v4 as uuidv4 } from 'uuid';
+import Head from "next/head";
 const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState<string | undefined>("Threads");
   const filteredData = data.filter((item) => item.category === selectedCategory || !selectedCategory);
 
   return (
     <div className="font-jetbrains">
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🛡️</text></svg>"
+        />
+      </Head>
       <Hero />
       <Navbar setCategory={setSelectedCategory} />
       <div className="grid grid-cols-1 mt-10 sm:grid-cols-2 lg:grid-cols-3 gap-4 place-items-center">
