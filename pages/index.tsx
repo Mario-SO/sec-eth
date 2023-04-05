8da0e98cab626a3af40ec1352ec2edb330071f72
Mario-SO <gh_mario.proclaim@slmail.me>
2023-04-05 19:47:39 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ const Home = () => {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🛡️</text></svg>"
         />
+        <title>sec-eth.xyz</title>
       </Head>
       <Hero />
       <Navbar setCategory={setSelectedCategory} />
