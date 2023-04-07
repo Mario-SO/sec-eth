6840e371a1a49f17f2f732aca50807a1a6fbcdc1
Mario-SO <gh_mario.proclaim@slmail.me>
2023-04-07 18:42:49 +0200

    Update Hero.tsx

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
             <div className="container mx-auto px-4">
                 <div className="flex flex-col justify-center items-center my-10">
                     <h1 className="text-5xl font-bold text-gray-900">⚔️ Welcome to <span className="text-blue-500">sec-eth🛡️</span></h1>
-                    <p className="text-2xl text-gray-500 mt-4">Securing Web3.</p>
+                    <p className="text-2xl text-gray-500 mt-4">Securing Web3 - Made by <a className="text-blue-500" href="https://twitter.com/mariodev__">@mariodev__</a></p>
                 </div>
             </div>
         </div>
